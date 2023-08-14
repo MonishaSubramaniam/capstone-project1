d5b83c9ac09300cda2b739502bb33ab7d695c9da
Monisha <marvelousmonik@gmail.com>
2023-08-14 13:26:25 -0700

    v1-add all files

---
+++ Test_Locators/locators.py
@@ -0,0 +1,36 @@
+# locators.py
+# To keep the Locators data
+class Locators:
+    username_input_box = "//input[@placeholder='Username']" # NAME
+    password_input_box = "//input[@placeholder='Password']" # NAME
+    submit_button = "//button[@type='submit']" #xpath
+    invalid_credentials='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]'
+class pimloc:
+    pim='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'      #xpath
+    add='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button'  #xpath
+    fname='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/input'   #xpath                                                
+    mname='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/input'   #xpath                                          
+    lname="//input[@placeholder='Last Name']"   #xpath                                                  
+    save='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]' #xpath
+    add_employee='Add Employee' #link
+    emplist='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input'           #xpath                                       
+    nick='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[2]/div/div/div[2]/input'         #xpath
+    license='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input'    #xpath
+    other='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input'
+    ssn='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[1]/div/div[2]/input'
+    sin='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[2]/div/div[2]/input'
+    date='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/div/input'
+    save1='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
+    nationality='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div/div[1]'
+    afghan='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div/div[1]'
+    marital='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div/div[1]'
+    single='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div/div[1]'
+    female='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/label'
+    dob='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/div/input'
+    emp_list='Employee List'
+    emp_name='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input'
+    search_btn='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]'
+    edit="//div[@role='table']//div[1]//div[1]//div[9]//div[1]//button[2]//i[1]"
+    delete="//button[normalize-space()='Delete Selected']"
+    check="//div[@class='oxd-table-card-cell-checkbox']//i[@class='oxd-icon bi-check oxd-checkbox-input-icon']"
+    confirm_delete="//button[normalize-space()='Yes, Delete']"
